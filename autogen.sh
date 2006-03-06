4ac9ab6a3f61034e18353303b4cd8dbb0473e287
Benedikt Meurer <benny@xfce.org>
2006-03-06 15:47:21 +0000

    2006-03-06	Benedikt Meurer <benny@xfce.org>
    	* autogen.sh, configure.in.in, po/LINGUAS: Read the set of available
    	  languages from po/LINGUAS and substitute them when autogen.sh is
    	  run. This way we can continue to use glib-gettext.
    
    
    
    
    (Old svn revision: 20257)

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,9 @@
 # $Id$
 #
 # Copyright (c) 2002-2006
-#         The Xfce development team. All rights reserved.
+#         The Thunar development team. All rights reserved.
 #
-# Written for Xfce by Benedikt Meurer <benny@xfce.org>.
+# Written for Thunar by Benedikt Meurer <benny@xfce.org>.
 #
 
 (type xdt-autogen) >/dev/null 2>&1 || {
@@ -18,9 +18,22 @@ EOF
   exit 1
 }
 
-# substitute revision and date
+# verify that po/LINGUAS is present
+(test -f po/LINGUAS) >/dev/null 2>&1 || {
+  cat >&2 <<EOF
+autogen.sh: The file po/LINGUAS could not be found. Please check your snapshot
+            or try to checkout again.
+EOF
+  exit 1
+}
+
+# substitute revision and linguas
+linguas=`sed -e '/^#/d' po/LINGUAS`
 revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}'`
-sed -e "s/@REVISION@/${revision}/g" < "configure.in.in" > "configure.in"
+sed -e "s/@LINGUAS@/${linguas}/g" \
+    -e "s/@REVISION@/${revision}/g" \
+    < "configure.in.in" > "configure.in"
+
 exec xdt-autogen $@
 
 # vi:set ts=2 sw=2 et ai:
