5fd3aa42945969ffe4f2d9c2bedc5580a5b8d6f1
Nick Schermer <nick@xfce.org>
2012-12-23 22:35:12 +0100

    Move glade file in new location.

--- terminal/terminal-preferences-dialog.c
+++ terminal/terminal-preferences-dialog.c
@@ -146,7 +146,7 @@ terminal_preferences_dialog_init (TerminalPreferencesDialog *dialog)
 
   /* lookup the ui file */
   xfce_resource_push_path (XFCE_RESOURCE_DATA, DATADIR);
-  file = xfce_resource_lookup (XFCE_RESOURCE_DATA, "xfce4-terminal/xfce4-terminal.glade");
+  file = xfce_resource_lookup (XFCE_RESOURCE_DATA, "xfce4/terminal/terminal-preferences.glade");
   xfce_resource_pop_path (XFCE_RESOURCE_DATA);
 
   if (G_UNLIKELY (file == NULL))
