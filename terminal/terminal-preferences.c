4c9d09d6c23226174476c713b0667aec7f463e81
Nick Schermer <nick@xfce.org>
2012-12-26 11:21:37 +0100

    Make the default cursor color same as the palette.

--- terminal/terminal-preferences.c
+++ terminal/terminal-preferences.c
@@ -371,7 +371,7 @@ terminal_preferences_class_init (TerminalPreferencesClass *klass)
       g_param_spec_string ("color-cursor",
                            "color-cursor",
                            "ColorCursor",
-                           "#00ff00",
+                           "#00aa00",
                            G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
 
   /**
