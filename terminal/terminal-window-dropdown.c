e64567089cdc30f68fda884c706799118ce948dd
Nick Schermer <nick@xfce.org>
2012-12-30 18:48:49 +0100

    Drop the dialog hint, not working.
    Xfwm4 associates the window with other terminal windows.

--- terminal/terminal-window-dropdown.c
+++ terminal/terminal-window-dropdown.c
@@ -253,7 +253,6 @@ terminal_window_dropdown_init (TerminalWindowDropdown *dropdown)
   gtk_window_set_resizable (GTK_WINDOW (dropdown), FALSE);
   gtk_window_set_decorated (GTK_WINDOW (dropdown), FALSE);
   gtk_window_set_gravity (GTK_WINDOW (dropdown), GDK_GRAVITY_STATIC);
-  gtk_window_set_type_hint  (GTK_WINDOW (dropdown), GDK_WINDOW_TYPE_HINT_DIALOG); /* avoids smart placement */
   gtk_window_stick (GTK_WINDOW (dropdown));
 
   /* this avoids to return focus to the window after dialog changes,
