d99aef8215bd978dd2128673f6a4bbb1e1790283
Nick Schermer <nick@xfce.org>
2012-12-31 17:42:53 +0100

    Fixup licenses (bug #9697).
    Also drop ref to gnome-terminal since this code
    is no longer used.

--- terminal/terminal-window-dropdown.c
+++ terminal/terminal-window-dropdown.c
@@ -11,12 +11,8 @@
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA  02111-1307  USA.
- *
- * The geometry handling code was taken from gnome-terminal. The geometry hacks
- * were initially written by Owen Taylor.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifdef HAVE_CONFIG_H
