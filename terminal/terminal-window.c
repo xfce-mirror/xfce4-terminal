f0c77ec63b92ea31e3097704438d7d4f0b5e1bf6
Igor <f2404@yandex.ru>
2016-06-23 13:10:27 +0300

    Fix tab remove handling: context menu and keyboard shortcuts were not applied to proper tab
    Fixes #11227

--- terminal/terminal-window.c
+++ terminal/terminal-window.c
@@ -926,16 +926,16 @@ terminal_window_notebook_page_removed (GtkNotebook    *notebook,
       return;
     }
 
-  /* send a signal about switching to another tab */
-  new_page_num = gtk_notebook_get_current_page (GTK_NOTEBOOK (window->notebook));
-  new_page = gtk_notebook_get_nth_page (GTK_NOTEBOOK (window->notebook), new_page_num);
-  terminal_window_notebook_page_switched (notebook, new_page, new_page_num, window);
-
   /* show the tabs when needed */
   terminal_window_notebook_show_tabs (window);
 
   /* regenerate the "Go" menu */
   terminal_window_rebuild_tabs_menu (window);
+
+  /* send a signal about switching to another tab */
+  new_page_num = gtk_notebook_get_current_page (GTK_NOTEBOOK (window->notebook));
+  new_page = gtk_notebook_get_nth_page (GTK_NOTEBOOK (window->notebook), new_page_num);
+  terminal_window_notebook_page_switched (notebook, new_page, new_page_num, window);
 }
 
 static gboolean
