efd7014503ff6f6530c93766bbb035a1afbfe48d
Igor <f2404@yandex.ru>
2016-06-25 21:15:49 +0300

    Do not prevent the terminal from being resized in drop-down mode
    This caused weird issues when the terminal could be increased in size but
    couldn't be decreased, even to its original size.
    It seems the intention was to prevent the user from being able to resize
    the terminal; to me, this is still impossible even after removing the code line.

--- terminal/terminal-window-dropdown.c
+++ terminal/terminal-window-dropdown.c
@@ -243,7 +243,6 @@ terminal_window_dropdown_init (TerminalWindowDropdown *dropdown)
   gtk_widget_show (dropdown->viewport);
 
   /* default window settings */
-  gtk_window_set_resizable (GTK_WINDOW (dropdown), FALSE);
   gtk_window_set_decorated (GTK_WINDOW (dropdown), FALSE);
   gtk_window_set_gravity (GTK_WINDOW (dropdown), GDK_GRAVITY_STATIC);
   gtk_window_set_type_hint (GTK_WINDOW (dropdown), GDK_WINDOW_TYPE_HINT_NORMAL);
