18509a14af70aef48bf216a3b2df4edb353ab2b0
Igor <f2404@yandex.ru>
2016-07-15 16:52:23 +0300

    Prevent characters from being hidden by block cursor
    When text and cursor colors are similar (or close).
    Fixes #9348

--- terminal/terminal-screen.c
+++ terminal/terminal-screen.c
@@ -941,6 +941,7 @@ terminal_screen_update_colors (TerminalScreen *screen)
   /* cursor color */
   has_cursor = terminal_preferences_get_color (screen->preferences, "color-cursor", &cursor);
   vte_terminal_set_color_cursor (VTE_TERMINAL (screen->terminal), has_cursor ? &cursor : NULL);
+  vte_terminal_set_color_cursor_foreground (VTE_TERMINAL (screen->terminal), has_bg ? &bg : NULL);
 
   /* selection color */
   if (!selection_use_default)
