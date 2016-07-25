1fc4099fc5c11f03dac3ac44b6bf0d1bda9f7900
Igor <f2404@yandex.ru>
2016-07-25 17:41:13 +0300

    Fix typo in the usage text

--- terminal/main.c
+++ terminal/main.c
@@ -131,7 +131,7 @@ usage (void)
            "  --display=%s; --geometry=%s; --role=%s; --drop-down;\n"
            "  --startup-id=%s; -I, --icon=%s; --fullscreen; --maximize;\n"
            "  --show-menubar, --hide-menubar; --show-borders, --hide-borders;\n"
-           "  --show-toolbar, --hide-toolbar, --font=%s, --zoom=%s\n\n",
+           "  --show-toolbar, --hide-toolbar; --font=%s, --zoom=%s\n\n",
            _("Window Options"),
            /* parameter of --display */
            _("display"),
