fdc57ae847bd4bca62c0f87466cc77feaa4e1cce
Igor <f2404@yandex.ru>
2016-10-28 18:28:51 +0300

    Restore deprecated gdk_keyboard_(un)grab
    To avoid flickering when hiding the window under xfwm4.

--- terminal/terminal-window-dropdown.c
+++ terminal/terminal-window-dropdown.c
@@ -421,26 +421,12 @@ static gboolean
 terminal_window_dropdown_can_grab (gpointer data)
 {
   GdkWindow    *window = gtk_widget_get_window (GTK_WIDGET (data));
-  GdkGrabStatus status;
-
-#if GTK_CHECK_VERSION (3, 20, 0)
-  GdkSeat *seat = gdk_display_get_default_seat (gtk_widget_get_display (GTK_WIDGET (data)));
-  if (!gdk_window_is_visible (window))
-    return FALSE;
-  status = gdk_seat_grab (seat, window, GDK_SEAT_CAPABILITY_KEYBOARD, FALSE, NULL, NULL, NULL, NULL);
-#else
-  status = gdk_keyboard_grab (window, FALSE, GDK_CURRENT_TIME);
-#endif
+  GdkGrabStatus status = gdk_keyboard_grab (window, FALSE, GDK_CURRENT_TIME);
 
   if (status == GDK_GRAB_SUCCESS)
     {
       /* drop the grab */
-#if GTK_CHECK_VERSION (3, 20, 0)
-      gdk_seat_ungrab (seat);
-#else
       gdk_keyboard_ungrab (GDK_CURRENT_TIME);
-#endif
-
       return FALSE;
     }
 
