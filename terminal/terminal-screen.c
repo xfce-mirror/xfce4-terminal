13c71c81eb54db919ca23c870a9af1cb92233dca
Igor <f2404@yandex.ru>
2017-05-30 20:34:02 -0400

    Fix negative dimensions GTK warning when running terminal with --tab option

--- terminal/terminal-screen.c
+++ terminal/terminal-screen.c
@@ -434,9 +434,12 @@ terminal_screen_realize (GtkWidget *widget)
 
   (*GTK_WIDGET_CLASS (terminal_screen_parent_class)->realize) (widget);
 
-  /* make sure the TerminalWidget is realized as well */
+#if !GTK_CHECK_VERSION (3, 20, 0)
+  /* make sure the TerminalWidget is realized as well
+     (produces "Drawing a gadget with negative dimensions" Gtk-WARNING) */
   if (!gtk_widget_get_realized (TERMINAL_SCREEN (widget)->terminal))
     gtk_widget_realize (TERMINAL_SCREEN (widget)->terminal);
+#endif
 
   /* connect to the "composited-changed" signal */
   screen = gtk_widget_get_screen (widget);
