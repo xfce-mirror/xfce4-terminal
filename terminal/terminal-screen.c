15a3c22a19714b5ba0e5921f7361f4ff55f7724e
Gaël Bonithon <gael@xfce.org>
2021-01-27 12:29:56 +0100

    Fix compilation warnings

--- terminal/terminal-screen.c
+++ terminal/terminal-screen.c
@@ -3004,7 +3004,7 @@ terminal_screen_feed_text (TerminalScreen *screen,
 const gchar *
 terminal_screen_get_custom_fg_color (TerminalScreen *screen)
 {
-  terminal_return_if_fail (TERMINAL_IS_SCREEN (screen));
+  terminal_return_val_if_fail (TERMINAL_IS_SCREEN (screen), NULL);
   return screen->custom_fg_color;
 }
 
@@ -3013,7 +3013,7 @@ terminal_screen_get_custom_fg_color (TerminalScreen *screen)
 const gchar *
 terminal_screen_get_custom_bg_color (TerminalScreen *screen)
 {
-  terminal_return_if_fail (TERMINAL_IS_SCREEN (screen));
+  terminal_return_val_if_fail (TERMINAL_IS_SCREEN (screen), NULL);
   return screen->custom_bg_color;
 }
 
@@ -3022,7 +3022,7 @@ terminal_screen_get_custom_bg_color (TerminalScreen *screen)
 const gchar *
 terminal_screen_get_custom_title_color (TerminalScreen *screen)
 {
-  terminal_return_if_fail (TERMINAL_IS_SCREEN (screen));
+  terminal_return_val_if_fail (TERMINAL_IS_SCREEN (screen), NULL);
   return screen->custom_title_color;
 }
 
