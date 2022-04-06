xfce-mirror/xfce4-terminal

commit 6bac5f5a69229b48c90547acf227e508b971723c

Fix compilation warnings

terminal/terminal-widget.c
@@ -467,7 +467,7 @@ terminal_widget_drag_data_received (GtkWidget        *widget,
                                     guint             info,
                                     guint             time)
 {
-  const guint16 *ucs;
+  const guchar  *ucs;
   GdkRGBA        color;
   GString       *str;
   GValue         value = { 0, };
@@ -517,9 +517,9 @@ terminal_widget_drag_data_received (GtkWidget        *widget,
       else
         {
           str = g_string_new (NULL);
-          ucs = (const guint16 *) gtk_selection_data_get_data (selection_data);
+          ucs = gtk_selection_data_get_data (selection_data);
           for (n = 0; n < gtk_selection_data_get_length (selection_data) / 2 && ucs[n] != '\n'; ++n)
-            g_string_append_unichar (str, (gunichar) ucs[n]);
+            g_string_append_unichar (str, ucs[n]);
           filename = g_filename_from_uri (str->str, NULL, NULL);
           if (filename != NULL)
             {

terminal/terminal-screen.c
@@ -1762,7 +1762,7 @@ terminal_screen_unsafe_paste_dialog_new (TerminalScreen *screen,
   label = gtk_label_new (_("Pasting this text to the terminal may be dangerous as it looks like "
                            "some commands may be executed, potentially involving root access ('sudo')."));
   gtk_label_set_line_wrap (GTK_LABEL (label), TRUE);
-  gtk_container_add (gtk_info_bar_get_content_area (GTK_INFO_BAR (infobar)), label);
+  gtk_container_add (GTK_CONTAINER (gtk_info_bar_get_content_area (GTK_INFO_BAR (infobar))), label);
   gtk_container_add (GTK_CONTAINER (box), infobar);
   gtk_container_set_border_width (GTK_CONTAINER (gtk_dialog_get_content_area (GTK_DIALOG (dialog))), 0);
   gtk_container_add (GTK_CONTAINER (gtk_dialog_get_content_area (GTK_DIALOG (dialog))), box);
@@ -3004,7 +3004,7 @@ terminal_screen_feed_text (TerminalScreen *screen,
 const gchar *
 terminal_screen_get_custom_fg_color (TerminalScreen *screen)
 {
-  terminal_return_if_fail (TERMINAL_IS_SCREEN (screen));
+  terminal_return_val_if_fail (TERMINAL_IS_SCREEN (screen), NULL);
   return screen->custom_fg_color;
 }
 
@@ -3013,7 +3013,7 @@ terminal_screen_get_custom_fg_color (TerminalScreen *screen)
 const gchar *
 terminal_screen_get_custom_bg_color (TerminalScreen *screen)
 {
-  terminal_return_if_fail (TERMINAL_IS_SCREEN (screen));
+  terminal_return_val_if_fail (TERMINAL_IS_SCREEN (screen), NULL);
   return screen->custom_bg_color;
 }
 
@@ -3022,7 +3022,7 @@ terminal_screen_get_custom_bg_color (TerminalScreen *screen)
 const gchar *
 terminal_screen_get_custom_title_color (TerminalScreen *screen)
 {
-  terminal_return_if_fail (TERMINAL_IS_SCREEN (screen));
+  terminal_return_val_if_fail (TERMINAL_IS_SCREEN (screen), NULL);
   return screen->custom_title_color;
 }
 

commit 6aca96bc77ff9cae6e4f952fe4aea0bfafb50a27

Regression: dragging a URL from Chromium ends up truncated (Issue #180)
Introduced in 6bac5f5a69229b48c90547acf227e508b971723c.

terminal/terminal-widget.c
@@ -543,7 +543,7 @@ terminal_widget_drag_data_received (GtkWidget        *widget,
         {
           str = g_string_new (NULL);
           ucs = gtk_selection_data_get_data (selection_data);
-          for (n = 0; n < gtk_selection_data_get_length (selection_data) / 2 && ucs[n] != '\n'; ++n)
+          for (n = 0; n < gtk_selection_data_get_length (selection_data) && ucs[n] != '\n'; ++n)
             g_string_append_unichar (str, ucs[n]);
           filename = g_filename_from_uri (str->str, NULL, NULL);
           if (filename != NULL)