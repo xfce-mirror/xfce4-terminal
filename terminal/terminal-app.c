xfce-mirror/xfce4-terminal

commit 858dd4d06ee0d1034a41fa95994d9cfa6308df23

Replace the deprecated GtkActionEntries with XfceGtkActionEntries (Issue #79)
Additionally the UI has been rebuilt, not using gtk_ui_manager.
Shared UI submenus are grouped into sections.
Button press events are not consumed by the active accelerators, so they are always passed to the
terminal widget. The terminal widget decides if they should be acted upon or ignored.
Some GtkActions remain (e.g. encoding action).

Issue #79
MR !31

terminal/terminal-app.c
@@ -255,7 +255,6 @@ terminal_app_update_windows_accels (gpointer user_data)
 
   for (lp = app->windows; lp != NULL; lp = lp->next)
     {
-      terminal_window_rebuild_tabs_menu (TERMINAL_WINDOW (lp->data));
       terminal_window_update_tab_key_accels (TERMINAL_WINDOW (lp->data), app->tab_key_accels);
     }
 

terminal/terminal-window.h
@@ -35,6 +35,72 @@ G_BEGIN_DECLS
 
 typedef struct _TerminalWindowPrivate TerminalWindowPrivate;
 
+/* #XfceGtkActionEntrys provided by this widget */
+typedef enum
+{
+    TERMINAL_WINDOW_ACTION_FILE_MENU,
+    TERMINAL_WINDOW_ACTION_NEW_TAB,
+    TERMINAL_WINDOW_ACTION_NEW_WINDOW,
+    TERMINAL_WINDOW_ACTION_UNDO_CLOSE_TAB,
+    TERMINAL_WINDOW_ACTION_DETACH_TAB,
+    TERMINAL_WINDOW_ACTION_CLOSE_TAB,
+    TERMINAL_WINDOW_ACTION_CLOSE_OTHER_TABS,
+    TERMINAL_WINDOW_ACTION_CLOSE_WINDOW,
+    TERMINAL_WINDOW_ACTION_EDIT_MENU,
+    TERMINAL_WINDOW_ACTION_COPY,
+#if VTE_CHECK_VERSION (0, 49, 2)
+    TERMINAL_WINDOW_ACTION_COPY_HTML,
+#endif
+    TERMINAL_WINDOW_ACTION_PASTE,
+    TERMINAL_WINDOW_ACTION_PASTE_ALT,
+    TERMINAL_WINDOW_ACTION_PASTE_SELECTION,
+    TERMINAL_WINDOW_ACTION_PASTE_SELECTION_ALT,
+    TERMINAL_WINDOW_ACTION_SELECT_ALL,
+    TERMINAL_WINDOW_ACTION_COPY_INPUT,
+    TERMINAL_WINDOW_ACTION_PREFERENCES,
+    TERMINAL_WINDOW_ACTION_VIEW_MENU,
+    TERMINAL_WINDOW_ACTION_ZOOM_IN,
+    TERMINAL_WINDOW_ACTION_ZOOM_IN_ALT,
+    TERMINAL_WINDOW_ACTION_ZOOM_OUT,
+    TERMINAL_WINDOW_ACTION_ZOOM_OUT_ALT,
+    TERMINAL_WINDOW_ACTION_ZOOM_RESET,
+    TERMINAL_WINDOW_ACTION_ZOOM_RESET_ALT,
+    TERMINAL_WINDOW_ACTION_TERMINAL_MENU,
+    TERMINAL_WINDOW_ACTION_SET_TITLE,
+    TERMINAL_WINDOW_ACTION_SET_TITLE_COLOR,
+    TERMINAL_WINDOW_ACTION_SEARCH,
+    TERMINAL_WINDOW_ACTION_SEARCH_NEXT,
+    TERMINAL_WINDOW_ACTION_SEARCH_PREV,
+    TERMINAL_WINDOW_ACTION_SAVE_CONTENTS,
+    TERMINAL_WINDOW_ACTION_RESET,
+    TERMINAL_WINDOW_ACTION_RESET_AND_CLEAR,
+    TERMINAL_WINDOW_ACTION_TABS_MENU,
+    TERMINAL_WINDOW_ACTION_PREV_TAB,
+    TERMINAL_WINDOW_ACTION_NEXT_TAB,
+    TERMINAL_WINDOW_ACTION_LAST_ACTIVE_TAB,
+    TERMINAL_WINDOW_ACTION_MOVE_TAB_LEFT,
+    TERMINAL_WINDOW_ACTION_MOVE_TAB_RIGHT,
+    TERMINAL_WINDOW_ACTION_HELP_MENU,
+    TERMINAL_WINDOW_ACTION_CONTENTS,
+    TERMINAL_WINDOW_ACTION_ABOUT,
+    TERMINAL_WINDOW_ACTION_ZOOM_MENU,
+    TERMINAL_WINDOW_ACTION_SHOW_MENUBAR,
+    TERMINAL_WINDOW_ACTION_SHOW_TOOLBAR,
+    TERMINAL_WINDOW_ACTION_SHOW_BORDERS,
+    TERMINAL_WINDOW_ACTION_FULLSCREEN,
+    TERMINAL_WINDOW_ACTION_READ_ONLY,
+    TERMINAL_WINDOW_ACTION_SCROLL_ON_OUTPUT,
+} TerminalWindowAction;
+
+typedef enum
+{
+    MENU_SECTION_ZOOM       = 1 << 0,
+    MENU_SECTION_SIGNAL     = 1 << 1,
+    MENU_SECTION_COPY       = 1 << 2,
+    MENU_SECTION_PASTE      = 1 << 3,
+    MENU_SECTION_VIEW       = 1 << 4,
+} MenuSections;
+
 typedef struct
 {
   GtkWindowClass parent_class;
@@ -43,6 +109,12 @@ typedef struct
 typedef struct
 {
   GtkWindow              parent_instance;
+
+  GtkWidget             *menubar;
+  GtkWidget             *toolbar;
+
+  gboolean               fullscreen_supported;
+  gboolean               is_fullscreen;
 
   TerminalWindowPrivate *priv;
 } TerminalWindow;
@@ -83,10 +155,6 @@ GtkWidget         *terminal_window_get_vbox                 (TerminalWindow     
 GtkWidget         *terminal_window_get_notebook             (TerminalWindow     *window);
 
 GtkWidget         *terminal_window_get_preferences_dialog   (TerminalWindow     *window);
-
-GtkAction         *terminal_window_get_action               (TerminalWindow     *window,
-                                                             const gchar        *action_name);
-
 const gchar       *terminal_window_get_font                 (TerminalWindow     *window);
 void               terminal_window_set_font                 (TerminalWindow     *window,
                                                              const gchar        *font);
@@ -107,13 +175,13 @@ gint               terminal_window_get_menubar_height       (TerminalWindow     
 
 gint               terminal_window_get_toolbar_height       (TerminalWindow     *window);
 
-void               terminal_window_rebuild_tabs_menu        (TerminalWindow     *window);
-
-void               terminal_window_action_show_menubar      (GtkToggleAction    *action,
-                                                             TerminalWindow     *window);
+void               terminal_window_action_show_menubar      (TerminalWindow     *window);
 
 void               terminal_window_update_tab_key_accels    (TerminalWindow     *window,
                                                              GSList             *tab_key_accels);
+
+XfceGtkActionEntry *terminal_window_get_action_entry         (TerminalWindow      *window,
+                                                              TerminalWindowAction action);
 
 G_END_DECLS
 

.gitignore
@@ -59,7 +59,6 @@ terminal/terminal-enum-types.h
 terminal/terminal-marshal.c
 terminal/terminal-marshal.h
 terminal/terminal-preferences-ui.h
-terminal/terminal-window-ui.h
 terminal/xfce4-terminal
 xfce4-terminal-default-apps.xml
 xfce4-terminal.desktop

terminal/Makefile.am
@@ -14,7 +14,6 @@ xfce4_terminal_built_sources = \
 	terminal-enum-types.h \
 	terminal-marshal.c \
 	terminal-marshal.h \
-	terminal-window-ui.h \
 	terminal-preferences-ui.h
 
 xfce4_terminal_headers = \
@@ -103,9 +102,6 @@ DISTCLEANFILES = \
 BUILT_SOURCES = \
 	$(xfce4_terminal_built_sources)
 
-terminal-window-ui.h: $(srcdir)/terminal-window-ui.xml Makefile
-	$(AM_V_GEN) xdt-csource --strip-comments --strip-content --static --name=terminal_window_ui $< >$@
-
 terminal-preferences-ui.h: $(srcdir)/terminal-preferences.glade Makefile
 	$(AM_V_GEN) xdt-csource --strip-comments --strip-content --static --name=terminal_preferences_ui $< >$@
 
@@ -159,7 +155,6 @@ clean-local:
 EXTRA_DIST = \
 	terminal-config.h.in \
 	terminal-marshal.list \
-	terminal-window-ui.xml \
 	terminal-preferences.glade
 
 # vi:set ts=8 sw=8 noet ai nocindent:

terminal/terminal-util.c
@@ -145,3 +145,12 @@ terminal_util_activate_window (GtkWindow *window)
   gtk_window_present (window);
 #endif
 }
+
+
+
+void
+terminal_util_free_data (gpointer  data,
+                         GClosure *closure)
+{
+  g_free (data);
+}

terminal/terminal-util.h
@@ -26,6 +26,9 @@ void terminal_util_show_about_dialog  (GtkWindow    *parent);
 
 void terminal_util_activate_window    (GtkWindow    *window);
 
+void terminal_util_free_data          (gpointer      data,
+                                       GClosure     *closure);
+
 G_END_DECLS
 
 #endif /* !TERMINAL_UTIL_H */

terminal/terminal-widget.c
@@ -628,6 +628,14 @@ terminal_widget_key_press_event (GtkWidget    *widget,
   gboolean       shortcuts_no_menukey;
   gboolean       shift_arrows_scroll;
   gdouble        value;
+  GtkAccelGroup *group     = gtk_accel_groups_from_object (G_OBJECT (gtk_widget_get_toplevel (widget)))->data;
+  const guint    modifiers = event->state & gtk_accelerator_get_default_mod_mask ();
+  guint          found;
+
+  /* check if the key combination is used by terminal-window, if so don't process it */
+  gtk_accel_group_query (group, event->keyval, modifiers, &found);
+  if (found)
+    return TRUE;
 
   /* determine current settings */
   g_object_get (G_OBJECT (TERMINAL_WIDGET (widget)->preferences),

commit 86abcfefec1c994e1fe7bb94fb615945507ba41c

shortcuts: Restore broken action toggle-menubar
Fixes: 858dd4d06ee0d1034a41fa95994d9cfa6308df23
Related: !31

terminal/terminal-app.c
@@ -55,7 +55,6 @@
 
 
 static void     terminal_app_finalize                 (GObject            *object);
-static void     terminal_app_update_accels            (TerminalApp        *app);
 static void     terminal_app_update_windows_accels    (gpointer            user_data);
 static gboolean terminal_app_accel_map_load           (gpointer            user_data);
 static gboolean terminal_app_accel_map_save           (gpointer            user_data);
@@ -129,15 +128,11 @@ static void
 terminal_app_init (TerminalApp *app)
 {
   app->preferences = terminal_preferences_get ();
-  g_signal_connect_swapped (G_OBJECT (app->preferences), "notify::shortcuts-no-menukey",
-                            G_CALLBACK (terminal_app_update_accels), app);
 
   /* remember the original menu bar accel */
   g_object_get (G_OBJECT (gtk_settings_get_default ()),
                 "gtk-menu-bar-accel", &app->initial_menu_bar_accel,
                 NULL);
-
-  terminal_app_update_accels (app);
 
   /* schedule accel map load and update windows when finished */
   app->accel_map_load_id = gdk_threads_add_idle_full (G_PRIORITY_LOW, terminal_app_accel_map_load, app,
@@ -173,7 +168,6 @@ terminal_app_finalize (GObject *object)
     }
   g_slist_free (app->windows);
 
-  g_signal_handlers_disconnect_by_func (G_OBJECT (app->preferences), G_CALLBACK (terminal_app_update_accels), app);
   g_object_unref (G_OBJECT (app->preferences));
 
   if (app->initial_menu_bar_accel != NULL)
@@ -183,27 +177,6 @@ terminal_app_finalize (GObject *object)
     g_object_unref (G_OBJECT (app->session_client));
 
   (*G_OBJECT_CLASS (terminal_app_parent_class)->finalize) (object);
-}
-
-
-
-static void
-terminal_app_update_accels (TerminalApp *app)
-{
-  gboolean        no_key;
-  GdkModifierType mod;
-  guint           key;
-
-  g_object_get (G_OBJECT (app->preferences),
-                "shortcuts-no-menukey", &no_key,
-                NULL);
-  g_object_set (G_OBJECT (gtk_settings_get_default ()),
-                "gtk-menu-bar-accel",
-                no_key ? NULL : app->initial_menu_bar_accel,
-                NULL);
-  gtk_accelerator_parse (app->initial_menu_bar_accel, &key, &mod);
-  gtk_accel_map_change_entry ("<Actions>/terminal-window/toggle-menubar",
-                              no_key ? 0 : key, no_key ? 0 : mod, TRUE);
 }
 
 

terminal/terminal-window.c
@@ -194,6 +194,7 @@ static gboolean     terminal_window_action_paste_selection        (TerminalWindo
 static gboolean     terminal_window_action_select_all             (TerminalWindow      *window);
 static gboolean     terminal_window_action_copy_input             (TerminalWindow      *window);
 static gboolean     terminal_window_action_prefs                  (TerminalWindow      *window);
+static gboolean     terminal_window_action_toggle_menubar         (TerminalWindow      *window);
 static gboolean     terminal_window_action_show_menubar           (TerminalWindow      *window);
 static gboolean     terminal_window_action_toggle_toolbar         (TerminalWindow      *window);
 static gboolean     terminal_window_action_toggle_borders         (TerminalWindow      *window);
@@ -357,6 +358,7 @@ static XfceGtkActionEntry action_entries[] =
     { TERMINAL_WINDOW_ACTION_CONTENTS,              "<Actions>/terminal-window/contents",              "F1",                        XFCE_GTK_IMAGE_MENU_ITEM, N_ ("_Contents"),                     N_ ("Display help contents"),             "help-browser",           G_CALLBACK (terminal_window_action_contents), },
     { TERMINAL_WINDOW_ACTION_ABOUT,                 "<Actions>/terminal-window/about",                 "",                          XFCE_GTK_IMAGE_MENU_ITEM, N_ ("_About"),                        NULL,                                     "help-about",             G_CALLBACK (terminal_window_action_about), },
     { TERMINAL_WINDOW_ACTION_ZOOM_MENU,             "<Actions>/terminal-window/zoom-menu",             "",                          XFCE_GTK_MENU_ITEM,       N_ ("_Zoom"),                         NULL,                                     NULL,                     NULL, },
+    { TERMINAL_WINDOW_ACTION_TOGGLE_MENUBAR,        "<Actions>/terminal-window/toggle-menubar",        "F10",                       XFCE_GTK_MENU_ITEM,       N_ ("Show Menubar Temporarily"),      "",                                       NULL,                     G_CALLBACK (terminal_window_action_toggle_menubar), },
     { TERMINAL_WINDOW_ACTION_SHOW_MENUBAR,          "<Actions>/terminal-window/show-menubar",          "",                          XFCE_GTK_CHECK_MENU_ITEM, N_ ("Show _Menubar"),                 N_ ("Show/hide the menubar"),             NULL,                     G_CALLBACK (terminal_window_action_show_menubar), },
     { TERMINAL_WINDOW_ACTION_SHOW_TOOLBAR,          "<Actions>/terminal-window/show-toolbar",          "",                          XFCE_GTK_CHECK_MENU_ITEM, N_ ("Show _Toolbar"),                 N_ ("Show/hide the toolbar"),             NULL,                     G_CALLBACK (terminal_window_action_toggle_toolbar), },
     { TERMINAL_WINDOW_ACTION_SHOW_BORDERS,          "<Actions>/terminal-window/show-borders",          "",                          XFCE_GTK_CHECK_MENU_ITEM, N_ ("Show Window _Borders"),          N_ ("Show/hide the window decorations"),  NULL,                     G_CALLBACK (terminal_window_action_toggle_borders), },
@@ -1808,6 +1810,42 @@ terminal_window_action_prefs (TerminalWindow *window)
     }
 
   return TRUE;
+}
+
+
+
+static void
+hide_menubar (GtkMenuShell *menubar)
+{
+  g_signal_handlers_disconnect_by_func (menubar, hide_menubar, NULL);
+  gtk_widget_hide (GTK_WIDGET (menubar));
+}
+
+
+
+static gboolean
+terminal_window_action_toggle_menubar (TerminalWindow *window)
+{
+  terminal_return_if_fail (TERMINAL_IS_WINDOW (window));
+
+  if (!gtk_widget_get_visible (window->menubar))
+    {
+      gboolean shortcuts_no_menukey;
+      g_object_get (G_OBJECT (window->priv->preferences),
+                    "shortcuts-no-menukey", &shortcuts_no_menukey,
+                    NULL);
+      if (!shortcuts_no_menukey)
+        {
+          GList *items = gtk_container_get_children (GTK_CONTAINER (window->menubar));
+          gtk_widget_show (window->menubar);
+          gtk_menu_shell_select_first (GTK_MENU_SHELL (window->menubar), TRUE);
+          g_signal_connect (window->menubar, "deactivate", G_CALLBACK (hide_menubar), NULL);
+          g_list_free (items);
+          return TRUE;
+        }
+    }
+
+  return FALSE;
 }
 
 

terminal/terminal-window.h
@@ -82,6 +82,7 @@ typedef enum
     TERMINAL_WINDOW_ACTION_CONTENTS,
     TERMINAL_WINDOW_ACTION_ABOUT,
     TERMINAL_WINDOW_ACTION_ZOOM_MENU,
+    TERMINAL_WINDOW_ACTION_TOGGLE_MENUBAR,
     TERMINAL_WINDOW_ACTION_SHOW_MENUBAR,
     TERMINAL_WINDOW_ACTION_SHOW_TOOLBAR,
     TERMINAL_WINDOW_ACTION_SHOW_BORDERS,