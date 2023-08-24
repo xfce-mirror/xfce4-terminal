c576281a780884f9ba1f6abe3d543a3a00f0b48d
Gaël Bonithon <gael@xfce.org>
2023-08-24 12:50:38 +0200

    app: Put GtkSettings:gtk-menu-bar-accel overwrite back in place
    Fixes: 86abcfefec1c994e1fe7bb94fb615945507ba41c
    Closes: #259

--- terminal/terminal-app.c
+++ terminal/terminal-app.c
@@ -56,6 +56,7 @@
 
 
 static void     terminal_app_finalize                 (GObject            *object);
+static void     terminal_app_update_accels            (TerminalApp        *app);
 static void     terminal_app_update_windows_accels    (gpointer            user_data);
 static gboolean terminal_app_accel_map_load           (gpointer            user_data);
 static gboolean terminal_app_accel_map_save           (gpointer            user_data);
@@ -129,11 +130,15 @@ static void
 terminal_app_init (TerminalApp *app)
 {
   app->preferences = terminal_preferences_get ();
+  g_signal_connect_swapped (G_OBJECT (app->preferences), "notify::shortcuts-no-menukey",
+                            G_CALLBACK (terminal_app_update_accels), app);
 
   /* remember the original menu bar accel */
   g_object_get (G_OBJECT (gtk_settings_get_default ()),
                 "gtk-menu-bar-accel", &app->initial_menu_bar_accel,
                 NULL);
+
+  terminal_app_update_accels (app);
 
   /* schedule accel map load and update windows when finished */
   app->accel_map_load_id = gdk_threads_add_idle_full (G_PRIORITY_LOW, terminal_app_accel_map_load, app,
@@ -169,6 +174,7 @@ terminal_app_finalize (GObject *object)
     }
   g_slist_free (app->windows);
 
+  g_signal_handlers_disconnect_by_func (G_OBJECT (app->preferences), G_CALLBACK (terminal_app_update_accels), app);
   g_object_unref (G_OBJECT (app->preferences));
 
   if (app->initial_menu_bar_accel != NULL)
@@ -178,6 +184,22 @@ terminal_app_finalize (GObject *object)
     g_object_unref (G_OBJECT (app->session_client));
 
   (*G_OBJECT_CLASS (terminal_app_parent_class)->finalize) (object);
+}
+
+
+
+static void
+terminal_app_update_accels (TerminalApp *app)
+{
+  gboolean no_key;
+
+  g_object_get (G_OBJECT (app->preferences),
+                "shortcuts-no-menukey", &no_key,
+                NULL);
+  g_object_set (G_OBJECT (gtk_settings_get_default ()),
+                "gtk-menu-bar-accel",
+                no_key ? NULL : app->initial_menu_bar_accel,
+                NULL);
 }
 
 

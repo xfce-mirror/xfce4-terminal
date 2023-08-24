xfce-mirror/xfce4-terminal

commit 86abcfefec1c994e1fe7bb94fb615945507ba41c

shortcuts: Restore broken action toggle-menubar
Fixes: 858dd4d06ee0d1034a41fa95994d9cfa6308df23
Related: !31

terminal/terminal-app.c
@@ -55,7 +55,6 @@
 
 
 static void     terminal_app_finalize                 (GObject            *object);
-static void     terminal_app_update_accels            (TerminalApp        *app);
 static void     terminal_app_update_windows_accels    (gpointer            user_data);
 static gboolean terminal_app_accel_map_load           (gpointer            user_data);
 static gboolean terminal_app_accel_map_save           (gpointer            user_data);
@@ -129,15 +128,11 @@ static void
 terminal_app_init (TerminalApp *app)
 {
   app->preferences = terminal_preferences_get ();
-  g_signal_connect_swapped (G_OBJECT (app->preferences), "notify::shortcuts-no-menukey",
-                            G_CALLBACK (terminal_app_update_accels), app);
 
   /* remember the original menu bar accel */
   g_object_get (G_OBJECT (gtk_settings_get_default ()),
                 "gtk-menu-bar-accel", &app->initial_menu_bar_accel,
                 NULL);
-
-  terminal_app_update_accels (app);
 
   /* schedule accel map load and update windows when finished */
   app->accel_map_load_id = gdk_threads_add_idle_full (G_PRIORITY_LOW, terminal_app_accel_map_load, app,
@@ -173,7 +168,6 @@ terminal_app_finalize (GObject *object)
     }
   g_slist_free (app->windows);
 
-  g_signal_handlers_disconnect_by_func (G_OBJECT (app->preferences), G_CALLBACK (terminal_app_update_accels), app);
   g_object_unref (G_OBJECT (app->preferences));
 
   if (app->initial_menu_bar_accel != NULL)
@@ -183,27 +177,6 @@ terminal_app_finalize (GObject *object)
     g_object_unref (G_OBJECT (app->session_client));
 
   (*G_OBJECT_CLASS (terminal_app_parent_class)->finalize) (object);
-}
-
-
-
-static void
-terminal_app_update_accels (TerminalApp *app)
-{
-  gboolean        no_key;
-  GdkModifierType mod;
-  guint           key;
-
-  g_object_get (G_OBJECT (app->preferences),
-                "shortcuts-no-menukey", &no_key,
-                NULL);
-  g_object_set (G_OBJECT (gtk_settings_get_default ()),
-                "gtk-menu-bar-accel",
-                no_key ? NULL : app->initial_menu_bar_accel,
-                NULL);
-  gtk_accelerator_parse (app->initial_menu_bar_accel, &key, &mod);
-  gtk_accel_map_change_entry ("<Actions>/terminal-window/toggle-menubar",
-                              no_key ? 0 : key, no_key ? 0 : mod, TRUE);
 }
 
 

terminal/terminal-window.c
@@ -194,6 +194,7 @@ static gboolean     terminal_window_action_paste_selection        (TerminalWindo
 static gboolean     terminal_window_action_select_all             (TerminalWindow      *window);
 static gboolean     terminal_window_action_copy_input             (TerminalWindow      *window);
 static gboolean     terminal_window_action_prefs                  (TerminalWindow      *window);
+static gboolean     terminal_window_action_toggle_menubar         (TerminalWindow      *window);
 static gboolean     terminal_window_action_show_menubar           (TerminalWindow      *window);
 static gboolean     terminal_window_action_toggle_toolbar         (TerminalWindow      *window);
 static gboolean     terminal_window_action_toggle_borders         (TerminalWindow      *window);
@@ -357,6 +358,7 @@ static XfceGtkActionEntry action_entries[] =
     { TERMINAL_WINDOW_ACTION_CONTENTS,              "<Actions>/terminal-window/contents",              "F1",                        XFCE_GTK_IMAGE_MENU_ITEM, N_ ("_Contents"),                     N_ ("Display help contents"),             "help-browser",           G_CALLBACK (terminal_window_action_contents), },
     { TERMINAL_WINDOW_ACTION_ABOUT,                 "<Actions>/terminal-window/about",                 "",                          XFCE_GTK_IMAGE_MENU_ITEM, N_ ("_About"),                        NULL,                                     "help-about",             G_CALLBACK (terminal_window_action_about), },
     { TERMINAL_WINDOW_ACTION_ZOOM_MENU,             "<Actions>/terminal-window/zoom-menu",             "",                          XFCE_GTK_MENU_ITEM,       N_ ("_Zoom"),                         NULL,                                     NULL,                     NULL, },
+    { TERMINAL_WINDOW_ACTION_TOGGLE_MENUBAR,        "<Actions>/terminal-window/toggle-menubar",        "F10",                       XFCE_GTK_MENU_ITEM,       N_ ("Show Menubar Temporarily"),      "",                                       NULL,                     G_CALLBACK (terminal_window_action_toggle_menubar), },
     { TERMINAL_WINDOW_ACTION_SHOW_MENUBAR,          "<Actions>/terminal-window/show-menubar",          "",                          XFCE_GTK_CHECK_MENU_ITEM, N_ ("Show _Menubar"),                 N_ ("Show/hide the menubar"),             NULL,                     G_CALLBACK (terminal_window_action_show_menubar), },
     { TERMINAL_WINDOW_ACTION_SHOW_TOOLBAR,          "<Actions>/terminal-window/show-toolbar",          "",                          XFCE_GTK_CHECK_MENU_ITEM, N_ ("Show _Toolbar"),                 N_ ("Show/hide the toolbar"),             NULL,                     G_CALLBACK (terminal_window_action_toggle_toolbar), },
     { TERMINAL_WINDOW_ACTION_SHOW_BORDERS,          "<Actions>/terminal-window/show-borders",          "",                          XFCE_GTK_CHECK_MENU_ITEM, N_ ("Show Window _Borders"),          N_ ("Show/hide the window decorations"),  NULL,                     G_CALLBACK (terminal_window_action_toggle_borders), },
@@ -1808,6 +1810,42 @@ terminal_window_action_prefs (TerminalWindow *window)
     }
 
   return TRUE;
+}
+
+
+
+static void
+hide_menubar (GtkMenuShell *menubar)
+{
+  g_signal_handlers_disconnect_by_func (menubar, hide_menubar, NULL);
+  gtk_widget_hide (GTK_WIDGET (menubar));
+}
+
+
+
+static gboolean
+terminal_window_action_toggle_menubar (TerminalWindow *window)
+{
+  terminal_return_if_fail (TERMINAL_IS_WINDOW (window));
+
+  if (!gtk_widget_get_visible (window->menubar))
+    {
+      gboolean shortcuts_no_menukey;
+      g_object_get (G_OBJECT (window->priv->preferences),
+                    "shortcuts-no-menukey", &shortcuts_no_menukey,
+                    NULL);
+      if (!shortcuts_no_menukey)
+        {
+          GList *items = gtk_container_get_children (GTK_CONTAINER (window->menubar));
+          gtk_widget_show (window->menubar);
+          gtk_menu_shell_select_first (GTK_MENU_SHELL (window->menubar), TRUE);
+          g_signal_connect (window->menubar, "deactivate", G_CALLBACK (hide_menubar), NULL);
+          g_list_free (items);
+          return TRUE;
+        }
+    }
+
+  return FALSE;
 }
 
 

terminal/terminal-window.h
@@ -82,6 +82,7 @@ typedef enum
     TERMINAL_WINDOW_ACTION_CONTENTS,
     TERMINAL_WINDOW_ACTION_ABOUT,
     TERMINAL_WINDOW_ACTION_ZOOM_MENU,
+    TERMINAL_WINDOW_ACTION_TOGGLE_MENUBAR,
     TERMINAL_WINDOW_ACTION_SHOW_MENUBAR,
     TERMINAL_WINDOW_ACTION_SHOW_TOOLBAR,
     TERMINAL_WINDOW_ACTION_SHOW_BORDERS,

commit c576281a780884f9ba1f6abe3d543a3a00f0b48d

app: Put GtkSettings:gtk-menu-bar-accel overwrite back in place
Fixes: 86abcfefec1c994e1fe7bb94fb615945507ba41c
Closes: #259

terminal/terminal-app.c
@@ -56,6 +56,7 @@
 
 
 static void     terminal_app_finalize                 (GObject            *object);
+static void     terminal_app_update_accels            (TerminalApp        *app);
 static void     terminal_app_update_windows_accels    (gpointer            user_data);
 static gboolean terminal_app_accel_map_load           (gpointer            user_data);
 static gboolean terminal_app_accel_map_save           (gpointer            user_data);
@@ -129,11 +130,15 @@ static void
 terminal_app_init (TerminalApp *app)
 {
   app->preferences = terminal_preferences_get ();
+  g_signal_connect_swapped (G_OBJECT (app->preferences), "notify::shortcuts-no-menukey",
+                            G_CALLBACK (terminal_app_update_accels), app);
 
   /* remember the original menu bar accel */
   g_object_get (G_OBJECT (gtk_settings_get_default ()),
                 "gtk-menu-bar-accel", &app->initial_menu_bar_accel,
                 NULL);
+
+  terminal_app_update_accels (app);
 
   /* schedule accel map load and update windows when finished */
   app->accel_map_load_id = gdk_threads_add_idle_full (G_PRIORITY_LOW, terminal_app_accel_map_load, app,
@@ -169,6 +174,7 @@ terminal_app_finalize (GObject *object)
     }
   g_slist_free (app->windows);
 
+  g_signal_handlers_disconnect_by_func (G_OBJECT (app->preferences), G_CALLBACK (terminal_app_update_accels), app);
   g_object_unref (G_OBJECT (app->preferences));
 
   if (app->initial_menu_bar_accel != NULL)
@@ -178,6 +184,22 @@ terminal_app_finalize (GObject *object)
     g_object_unref (G_OBJECT (app->session_client));
 
   (*G_OBJECT_CLASS (terminal_app_parent_class)->finalize) (object);
+}
+
+
+
+static void
+terminal_app_update_accels (TerminalApp *app)
+{
+  gboolean no_key;
+
+  g_object_get (G_OBJECT (app->preferences),
+                "shortcuts-no-menukey", &no_key,
+                NULL);
+  g_object_set (G_OBJECT (gtk_settings_get_default ()),
+                "gtk-menu-bar-accel",
+                no_key ? NULL : app->initial_menu_bar_accel,
+                NULL);
 }
 
 