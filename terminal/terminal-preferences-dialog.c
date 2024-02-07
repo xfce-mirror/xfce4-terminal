xfce-mirror/xfce4-terminal

commit de3e7aac72fdcd3e62d69f37ec2570e5d668950a

build: Get rid of #ifdef G_ENABLE_DEBUG
It just accumulates code that nobody ever checks compiles, and which
therefore has every chance of not compiling the day someone wants to run
it. It's also more practical to use environment variables to
enable/disable debugging output or program abort on warnings. As for
assertions, they have their own disabling mechanism when
G_DISABLE_CHECKS is set, i.e. when --disable-debug is used at our level.

Fixes: #280

terminal/terminal-preferences-dialog.c
@@ -1692,7 +1692,7 @@ terminal_preferences_dialog_background_notify (TerminalPreferencesDialog *dialog
 {
   gchar *button_file, *prop_file;
 
-  terminal_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
+  g_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
 
   button_file = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog->file_chooser));
   g_object_get (G_OBJECT (dialog->preferences), "background-image-file", &prop_file, NULL);
@@ -1710,8 +1710,8 @@ terminal_preferences_dialog_background_set (TerminalPreferencesDialog *dialog,
 {
   gchar *filename;
 
-  terminal_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
-  terminal_return_if_fail (GTK_IS_FILE_CHOOSER_BUTTON (widget));
+  g_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
+  g_return_if_fail (GTK_IS_FILE_CHOOSER_BUTTON (widget));
 
   filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (widget));
   g_signal_handler_block (dialog->preferences, dialog->bg_image_signal_id);
@@ -1728,7 +1728,7 @@ terminal_preferences_dialog_geometry_notify (TerminalPreferencesDialog *dialog)
   gchar  *geometry = NULL;
   gchar **split = NULL;
 
-  terminal_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
+  g_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
 
   g_object_get (G_OBJECT (dialog->preferences), "misc-default-geometry", &geometry, NULL);
 
@@ -1764,8 +1764,8 @@ terminal_preferences_dialog_geometry_changed (TerminalPreferencesDialog *dialog,
   gint   cols;
   gchar *geometry;
 
-  terminal_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
-  terminal_return_if_fail (GTK_IS_SPIN_BUTTON (widget));
+  g_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
+  g_return_if_fail (GTK_IS_SPIN_BUTTON (widget));
 
   cols = gtk_spin_button_get_value_as_int (dialog->geometry_button[GEOMETRY_COLUMN]);
   rows = gtk_spin_button_get_value_as_int (dialog->geometry_button[GEOMETRY_ROW]);
@@ -1793,7 +1793,7 @@ terminal_preferences_dialog_palette_notify (TerminalPreferencesDialog *dialog)
   gchar    *color_str = NULL;
   gchar   **colors = NULL;
 
-  terminal_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
+  g_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
 
   g_object_get (dialog->preferences, "color-palette", &color_str, NULL);
 
@@ -1832,8 +1832,8 @@ terminal_preferences_dialog_palette_changed (TerminalPreferencesDialog *dialog,
   gchar   *color_str;
   GString *array;
 
-  terminal_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
-  terminal_return_if_fail (GTK_IS_COLOR_BUTTON (widget));
+  g_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
+  g_return_if_fail (GTK_IS_COLOR_BUTTON (widget));
 
   array = g_string_sized_new (225);
 
@@ -1893,8 +1893,8 @@ terminal_preferences_dialog_presets_changed (TerminalPreferencesDialog *dialog,
   GValue        src = { 0, };
   GValue        dst = { 0, };
 
-  terminal_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (widget));
-  terminal_return_if_fail (GTK_IS_COMBO_BOX (widget));
+  g_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (widget));
+  g_return_if_fail (GTK_IS_COMBO_BOX (widget));
 
   combobox = GTK_COMBO_BOX (widget);
 
@@ -1981,8 +1981,8 @@ terminal_preferences_dialog_presets_load (TerminalPreferencesDialog *dialog,
   const gchar  *title;
   gchar        *path;
 
-  terminal_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
-  terminal_return_if_fail (GTK_IS_COMBO_BOX (widget));
+  g_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
+  g_return_if_fail (GTK_IS_COMBO_BOX (widget));
 
   combobox = GTK_COMBO_BOX (widget);
 
@@ -2124,8 +2124,8 @@ terminal_preferences_dialog_encoding_changed (TerminalPreferencesDialog *dialog,
   GtkTreeIter   child_iter;
   GtkComboBox  *combobox;
 
-  terminal_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
-  terminal_return_if_fail (GTK_IS_COMBO_BOX (widget));
+  g_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
+  g_return_if_fail (GTK_IS_COMBO_BOX (widget));
 
   combobox = GTK_COMBO_BOX (widget);
 
@@ -2158,8 +2158,8 @@ terminal_gtk_label_set_a11y_relation (GtkLabel  *label,
   AtkRelation    *relation;
   AtkObject      *object;
 
-  terminal_return_if_fail (GTK_IS_WIDGET (widget));
-  terminal_return_if_fail (GTK_IS_LABEL (label));
+  g_return_if_fail (GTK_IS_WIDGET (widget));
+  g_return_if_fail (GTK_IS_LABEL (label));
 
   object = gtk_widget_get_accessible (widget);
   relations = atk_object_ref_relation_set (gtk_widget_get_accessible (GTK_WIDGET (label)));

terminal/main.c
@@ -197,13 +197,6 @@ main (int argc, char **argv)
 
   g_set_application_name (_("Xfce Terminal"));
 
-#ifdef G_ENABLE_DEBUG
-  /* Do NOT remove this line for now, If something doesn't work,
-   * fix your code instead!
-   */
-  g_log_set_always_fatal (G_LOG_LEVEL_CRITICAL | G_LOG_LEVEL_WARNING);
-#endif
-
   /* initialize GTK: do this before our parsing so GTK parses its options first */
   gtk_init (&argc, &argv);
 
@@ -297,11 +290,9 @@ main (int argc, char **argv)
               /* don't try to establish another service here */
               options.disable_server = 1;
 
-#ifdef G_ENABLE_DEBUG
               g_debug ("%s mismatch when invoking remote terminal: %s",
                        error->code == TERMINAL_ERROR_USER_MISMATCH ? "User" : "Display",
                        error->message);
-#endif
             }
           else if (g_error_matches (error, TERMINAL_ERROR, TERMINAL_ERROR_OPTIONS))
             {
@@ -318,13 +309,11 @@ main (int argc, char **argv)
               g_strfreev (nargv);
               return EXIT_FAILURE;
             }
-#ifdef G_ENABLE_DEBUG
           else if (error != NULL)
             {
               g_debug ("D-Bus reply error: %s (%s: %d)", error->message,
                        g_quark_to_string (error->domain), error->code);
             }
-#endif
 
           g_clear_error (&error);
         }

terminal/terminal-app.c
@@ -318,7 +318,7 @@ terminal_app_take_window (TerminalApp *app,
 {
   GtkWindowGroup *group;
 
-  terminal_return_if_fail (GTK_IS_WINDOW (window));
+  g_return_if_fail (GTK_IS_WINDOW (window));
 
   group = gtk_window_group_new ();
   gtk_window_group_add_window (group, window);
@@ -642,9 +642,9 @@ terminal_app_new_window_with_terminal (TerminalWindow *existing,
   GdkScreen *screen;
   glong      width, height;
 
-  terminal_return_if_fail (TERMINAL_IS_WINDOW (existing));
-  terminal_return_if_fail (TERMINAL_IS_SCREEN (terminal));
-  terminal_return_if_fail (TERMINAL_IS_APP (app));
+  g_return_if_fail (TERMINAL_IS_WINDOW (existing));
+  g_return_if_fail (TERMINAL_IS_SCREEN (terminal));
+  g_return_if_fail (TERMINAL_IS_APP (app));
 
   window = terminal_app_create_window (app, NULL, FALSE,
                                        TERMINAL_VISIBILITY_DEFAULT,
@@ -698,7 +698,7 @@ static void
 terminal_app_window_destroyed (GtkWidget   *window,
                                TerminalApp *app)
 {
-  terminal_return_if_fail (g_slist_find (app->windows, window) != NULL);
+  g_return_if_fail (g_slist_find (app->windows, window) != NULL);
 
   app->windows = g_slist_remove (app->windows, window);
 
@@ -744,7 +744,7 @@ terminal_app_save_yourself (XfceSMClient *client,
   oargv = xfce_sm_client_get_restart_command (client);
   if (oargv != NULL)
     {
-      terminal_assert (oargv[0] != NULL);
+      g_assert (oargv[0] != NULL);
       argv[0] = g_strdup (oargv[0]);
     }
   else
@@ -940,8 +940,8 @@ terminal_app_open_window (TerminalApp        *app,
   gint             screen_width = 0, screen_height = 0;
   gint             window_width, window_height;
 
-  terminal_return_if_fail (TERMINAL_IS_APP (app));
-  terminal_return_if_fail (attr != NULL);
+  g_return_if_fail (TERMINAL_IS_APP (app));
+  g_return_if_fail (attr != NULL);
 
   if (attr->drop_down)
     {

terminal/terminal-encoding-action.c
@@ -204,7 +204,7 @@ terminal_encoding_action_create_menu_item (GtkAction *action)
   GtkWidget *item;
   GtkWidget *menu;
 
-  terminal_return_val_if_fail (TERMINAL_IS_ENCODING_ACTION (action), NULL);
+  g_return_val_if_fail (TERMINAL_IS_ENCODING_ACTION (action), NULL);
 
   /* let GtkAction allocate the menu item */
 G_GNUC_BEGIN_IGNORE_DEPRECATIONS
@@ -227,7 +227,7 @@ terminal_encoding_action_activated (GtkWidget              *item,
 {
   const gchar *charset;
 
-  terminal_return_if_fail (GTK_IS_CHECK_MENU_ITEM (item));
+  g_return_if_fail (GTK_IS_CHECK_MENU_ITEM (item));
 
   if (!gtk_check_menu_item_get_active (GTK_CHECK_MENU_ITEM (item)))
     return;
@@ -258,8 +258,8 @@ terminal_encoding_action_menu_shown (GtkWidget              *menu,
   const gchar   *default_charset;
   gchar         *default_label;
 
-  terminal_return_if_fail (TERMINAL_IS_ENCODING_ACTION (action));
-  terminal_return_if_fail (GTK_IS_MENU_SHELL (menu));
+  g_return_if_fail (TERMINAL_IS_ENCODING_ACTION (action));
+  g_return_if_fail (GTK_IS_MENU_SHELL (menu));
 
   /* drop all existing children of the menu first */
   children = gtk_container_get_children (GTK_CONTAINER (menu));
@@ -348,8 +348,8 @@ GtkAction*
 terminal_encoding_action_new (const gchar *name,
                               const gchar *label)
 {
-  terminal_return_val_if_fail (name != NULL, NULL);
-  terminal_return_val_if_fail (label != NULL, NULL);
+  g_return_val_if_fail (name != NULL, NULL);
+  g_return_val_if_fail (label != NULL, NULL);
 
   return g_object_new (TERMINAL_TYPE_ENCODING_ACTION,
                        "hide-if-empty", FALSE,
@@ -366,7 +366,7 @@ terminal_encoding_action_set_charset (GtkAction   *gtkaction,
 {
   TerminalEncodingAction *action = TERMINAL_ENCODING_ACTION (gtkaction);
 
-  terminal_return_if_fail (TERMINAL_IS_ENCODING_ACTION (action));
+  g_return_if_fail (TERMINAL_IS_ENCODING_ACTION (action));
 
   g_free (action->current);
   action->current = g_strdup (charset);

terminal/terminal-gdbus.c
@@ -86,9 +86,9 @@ terminal_gdbus_method_call (GDBusConnection       *connection,
   GError       *error = NULL;
   gchar        *display_name2;
 
-  terminal_return_if_fail (TERMINAL_IS_APP (app));
-  terminal_return_if_fail (!g_strcmp0 (object_path, TERMINAL_DBUS_PATH));
-  terminal_return_if_fail (!g_strcmp0 (interface_name, TERMINAL_DBUS_INTERFACE));
+  g_return_if_fail (TERMINAL_IS_APP (app));
+  g_return_if_fail (!g_strcmp0 (object_path, TERMINAL_DBUS_PATH));
+  g_return_if_fail (!g_strcmp0 (interface_name, TERMINAL_DBUS_INTERFACE));
 
   if (g_strcmp0 (method_name, TERMINAL_DBUS_METHOD_LAUNCH) == 0)
     {
@@ -155,8 +155,8 @@ terminal_gdbus_bus_acquired (GDBusConnection *connection,
   GError        *error = NULL;
 
   info = g_dbus_node_info_new_for_xml (terminal_gdbus_introspection_xml, NULL);
-  terminal_assert (info != NULL);
-  terminal_assert (*info->interfaces != NULL);
+  g_assert (info != NULL);
+  g_assert (*info->interfaces != NULL);
 
   register_id = g_dbus_connection_register_object (connection,
                                                    TERMINAL_DBUS_PATH,
@@ -183,7 +183,7 @@ terminal_gdbus_register_service (TerminalApp *app,
 {
   guint owner_id;
 
-  terminal_return_val_if_fail (TERMINAL_IS_APP (app), FALSE);
+  g_return_val_if_fail (TERMINAL_IS_APP (app), FALSE);
 
   owner_id = g_bus_own_name (G_BUS_TYPE_SESSION,
                              TERMINAL_DBUS_SERVICE,
@@ -211,7 +211,7 @@ terminal_gdbus_invoke_launch (gint     argc,
   guint32          uid;
   gchar           *display_name;
 
-  terminal_return_val_if_fail (argc == (gint) g_strv_length (argv), FALSE);
+  g_return_val_if_fail (argc == (gint) g_strv_length (argv), FALSE);
 
   connection = g_bus_get_sync (G_BUS_TYPE_SESSION, NULL, error);
   if (G_UNLIKELY (connection == NULL))

terminal/terminal-image-loader.c
@@ -126,7 +126,7 @@ terminal_image_loader_check (TerminalImageLoader *loader)
   gchar                  *selected_color_spec;
   gchar                  *selected_path;
 
-  terminal_return_if_fail (TERMINAL_IS_IMAGE_LOADER (loader));
+  g_return_if_fail (TERMINAL_IS_IMAGE_LOADER (loader));
 
   g_object_get (G_OBJECT (loader->preferences),
                 "background-image-file", &selected_path,
@@ -417,20 +417,17 @@ terminal_image_loader_load (TerminalImageLoader *loader,
   GdkPixbuf *pixbuf;
   GSList    *lp;
 
-  terminal_return_val_if_fail (TERMINAL_IS_IMAGE_LOADER (loader), NULL);
-  terminal_return_val_if_fail (width > 0, NULL);
-  terminal_return_val_if_fail (height > 0, NULL);
+  g_return_val_if_fail (TERMINAL_IS_IMAGE_LOADER (loader), NULL);
+  g_return_val_if_fail (width > 0, NULL);
+  g_return_val_if_fail (height > 0, NULL);
 
   terminal_image_loader_check (loader);
 
   if (G_UNLIKELY (loader->pixbuf == NULL || width <= 1 || height <= 1))
     return NULL;
 
-#ifdef G_ENABLE_DEBUG
-  g_debug ("Image Loader Memory Status: %d images in valid "
-           "cache, %d in invalid cache",
-           g_slist_length (loader->cache),
-#endif
+  g_debug ("Image Loader Memory Status: %d images in valid cache",
+           g_slist_length (loader->cache));
 
   /* check for a cached version */
   for (lp = loader->cache; lp != NULL; lp = lp->next)
@@ -475,7 +472,7 @@ terminal_image_loader_load (TerminalImageLoader *loader,
       break;
 
     default:
-      terminal_assert_not_reached ();
+      g_assert_not_reached ();
     }
 
   loader->cache = g_slist_prepend (loader->cache, pixbuf);

terminal/terminal-options.c
@@ -101,7 +101,7 @@ terminal_option_cmp (const gchar   *long_name,
       return FALSE;
     }
 
-  terminal_assert (return_string != NULL);
+  g_assert (return_string != NULL);
   if (*(arg + offset) == '\0')
     *return_string = argv[++*argv_offset];
   else if (short_option)
@@ -126,8 +126,8 @@ terminal_option_show_hide_cmp (const gchar         *long_name,
   gchar *arg = argv[*argv_offset];
   const size_t pref_len = strlen ("--show-");
 
-  terminal_return_val_if_fail (long_name != NULL, FALSE);
-  terminal_return_val_if_fail (return_visibility != NULL, FALSE);
+  g_return_val_if_fail (long_name != NULL, FALSE);
+  g_return_val_if_fail (return_visibility != NULL, FALSE);
 
   if ((strncmp (arg, "--show-", pref_len) == 0 || strncmp (arg, "--hide-", pref_len) == 0)
       && strcmp (arg + pref_len, long_name) == 0)
@@ -734,7 +734,7 @@ terminal_tab_attr_new (void)
 void
 terminal_tab_attr_free (TerminalTabAttr *attr)
 {
-  terminal_return_if_fail (attr != NULL);
+  g_return_if_fail (attr != NULL);
 
   g_strfreev (attr->command);
   g_free (attr->directory);
@@ -755,7 +755,7 @@ terminal_tab_attr_free (TerminalTabAttr *attr)
 void
 terminal_window_attr_free (TerminalWindowAttr *attr)
 {
-  terminal_return_if_fail (attr != NULL);
+  g_return_if_fail (attr != NULL);
 
   g_slist_free_full (attr->tabs, (GDestroyNotify) terminal_tab_attr_free);
   g_free (attr->startup_id);

terminal/terminal-preferences.c
@@ -1318,7 +1318,7 @@ terminal_preferences_get_property (GObject    *object,
   gchar                 prop_name[64];
   gchar               **array;
 
-  terminal_return_if_fail (prop_id < N_PROPERTIES);
+  g_return_if_fail (prop_id < N_PROPERTIES);
 
   /* only set defaults if channel is not set */
   if (G_UNLIKELY (preferences->channel == NULL))
@@ -1454,11 +1454,6 @@ terminal_preferences_load_rc_file (TerminalPreferences *preferences)
       pspec = preferences_props[n];
       name = g_param_spec_get_name (pspec);
 
-#ifdef G_ENABLE_DEBUG
-      terminal_assert (g_param_spec_get_nick (pspec) != NULL);
-      terminal_preferences_check_blurb (pspec);
-#endif
-
       string = xfce_rc_read_entry (rc, g_param_spec_get_blurb (pspec), NULL);
       if (G_UNLIKELY (string == NULL))
         {
@@ -1522,46 +1517,6 @@ terminal_preferences_load_rc_file (TerminalPreferences *preferences)
 
 
 
-#ifdef G_ENABLE_DEBUG
-static void
-terminal_preferences_check_blurb (GParamSpec *spec)
-{
-  const gchar *s, *name;
-  gboolean     upper = TRUE;
-  gchar       *option, *t;
-
-  /* generate the option name */
-  name = g_param_spec_get_name (spec);
-  option = g_new (gchar, strlen (name) + 1);
-  for (s = name, t = option; *s != '\0'; ++s)
-    {
-      if (*s == '-')
-        {
-          upper = TRUE;
-        }
-      else if (upper)
-        {
-          *t++ = g_ascii_toupper (*s);
-          upper = FALSE;
-        }
-      else
-        {
-          *t++ = *s;
-        }
-    }
-  *t = '\0';
-
-  /* check if the generated option name is equal to the blurb */
-  if (g_strcmp0 (option, g_param_spec_get_blurb (spec)) != 0)
-    g_error ("The blurb of property \"%s\" does not match option name", name);
-
-  /* cleanup */
-  g_free (option);
-}
-#endif
-
-
-
 /**
  * terminal_preferences_get:
  *
@@ -1596,7 +1551,7 @@ terminal_preferences_get_color (TerminalPreferences *preferences,
   gchar    *spec;
   gboolean  succeed = FALSE;
 
-  terminal_return_val_if_fail (TERMINAL_IS_PREFERENCES (preferences), FALSE);
+  g_return_val_if_fail (TERMINAL_IS_PREFERENCES (preferences), FALSE);
 
   g_object_get (G_OBJECT (preferences), property, &spec, NULL);
   if (G_LIKELY (spec != NULL))

commit 71ecd2a721b059394edcfb316a20ac9d46a7b2cd

prefs-dialog: Fix wrong assert
Related: #299
Fixes: de3e7aac72fdcd3e62d69f37ec2570e5d668950a
Closes: #300

terminal/terminal-preferences-dialog.c
@@ -1893,7 +1893,7 @@ terminal_preferences_dialog_presets_changed (TerminalPreferencesDialog *dialog,
   GValue        src = { 0, };
   GValue        dst = { 0, };
 
-  g_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (widget));
+  g_return_if_fail (TERMINAL_IS_PREFERENCES_DIALOG (dialog));
   g_return_if_fail (GTK_IS_COMBO_BOX (widget));
 
   combobox = GTK_COMBO_BOX (widget);