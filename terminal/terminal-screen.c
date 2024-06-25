xfce-mirror/xfce4-terminal

commit bf4e5c3e272995dabe1a37f51fa953656e69ae17

Allow passing arguments to custom commands

terminal/terminal-screen.c
@@ -730,7 +730,17 @@ terminal_screen_get_child_command (TerminalScreen   *screen,
           g_object_get (G_OBJECT (screen->preferences),
                         "custom-command", &custom_command,
                         NULL);
-          shell_fullpath = custom_command;
+
+          if (!g_shell_parse_argv (custom_command, NULL, argv, error))
+            {
+              g_free (custom_command);
+              return FALSE;
+            }
+
+          *command = g_strdup (*argv[0]);
+
+          g_free (custom_command);
+          return TRUE;
         }
       else
         {
@@ -792,9 +802,6 @@ terminal_screen_get_child_command (TerminalScreen   *screen,
       else
         (*argv)[0] = g_strdup (shell_name);
       (*argv)[1] = NULL;
-
-      if (custom_command != NULL)
-        g_free (custom_command);
     }
 
   return TRUE;

commit 3fc52fe3d56a63b3f74c548a268383883160b3b4

Fix running a command as a login shell
Previous commit bf4e5c3e broke 2 things by returning too early when the
user defines a custom command:

- the "run as login shell" option becomes useless because we do not
  prepend argv0 with a "-" anymore
- argv0 itself should be the basename of the program run, but we don't
  do that anymore, and g_shell_parse_argv() does not do it either

This patch restores the old behavior by allowing the function to execute
to the end, even when defining a custom command.

terminal/terminal-screen.c
@@ -719,6 +719,7 @@ terminal_screen_get_child_command (TerminalScreen   *screen,
     }
   else
     {
+      *argv = NULL;
       g_object_get (G_OBJECT (screen->preferences),
                     "command-login-shell", &command_login_shell,
                     "run-custom-command", &run_custom_command,
@@ -737,10 +738,9 @@ terminal_screen_get_child_command (TerminalScreen   *screen,
               return FALSE;
             }
 
-          *command = g_strdup (*argv[0]);
+          shell_fullpath = *argv[0];
 
           g_free (custom_command);
-          return TRUE;
         }
       else
         {
@@ -796,7 +796,8 @@ terminal_screen_get_child_command (TerminalScreen   *screen,
         shell_name = shell_fullpath;
       *command = g_strdup (shell_fullpath);
 
-      *argv = g_new (gchar *, 2);
+      if (*argv == NULL)
+        *argv = g_new (gchar *, 2);
       if (command_login_shell)
         (*argv)[0] = g_strconcat ("-", shell_name, NULL);
       else