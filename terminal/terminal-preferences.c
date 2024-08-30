xfce-mirror/xfce4-terminal

commit 100f1991b672b93f61a49fddbb10b7115513b146

preferences: Check if channel itself exists instead of check property
This property could be reset, causing unwanted restoration of old
settings.

Related: !51

terminal/terminal-preferences.c
@@ -1269,7 +1269,7 @@ static void
 terminal_preferences_init (TerminalPreferences *preferences)
 {
   GError *error = NULL;
-  const gchar check_prop[] = "/title-initial";
+  gchar **channels;
 
   /* don't set a channel if xfconf init failed */
   if (!xfconf_init (&error))
@@ -1282,16 +1282,13 @@ terminal_preferences_init (TerminalPreferences *preferences)
   /* load the channel */
   preferences->channel = xfconf_channel_get ("xfce4-terminal");
 
-  /* check one of the property to see if there are values */
-  if (!xfconf_channel_has_property (preferences->channel, check_prop))
+  channels = xfconf_list_channels ();
+  if (!g_strv_contains ((const gchar * const *) channels, "xfce4-terminal"))
     {
       /* try to load the old config file & save changes */
       terminal_preferences_load_rc_file (preferences);
-
-      /* set the string we check */
-      if (!xfconf_channel_has_property (preferences->channel, check_prop))
-        xfconf_channel_set_string (preferences->channel, check_prop, _("Terminal"));
     }
+  g_strfreev (channels);
 
   preferences->property_changed_id =
     g_signal_connect (G_OBJECT (preferences->channel), "property-changed",

configure.ac.in
@@ -92,8 +92,8 @@ XDT_CHECK_LIBX11()
 dnl ***********************************
 dnl *** Check for required packages ***
 dnl ***********************************
-XDT_CHECK_PACKAGE([GLIB], [glib-2.0], [2.42.0])
-XDT_CHECK_PACKAGE([GIO], [gio-2.0], [2.42.0])
+XDT_CHECK_PACKAGE([GLIB], [glib-2.0], [2.44.0])
+XDT_CHECK_PACKAGE([GIO], [gio-2.0], [2.44.0])
 XDT_CHECK_PACKAGE([GTK], [gtk+-3.0], [3.22.0])
 XDT_CHECK_PACKAGE([VTE], [vte-2.91], [0.51.3])
 XDT_CHECK_PACKAGE([PCRE2], [libpcre2-8], [10.00])

commit 1fa6a60946bb84f46dbed930075164cb159dc6aa

preferences: Add missing sanity check
channels can be NULL exceptionally if there is an issue on dbus.

Fixes: 100f1991b672b93f61a49fddbb10b7115513b146

terminal/terminal-preferences.c
@@ -1292,7 +1292,7 @@ terminal_preferences_init (TerminalPreferences *preferences)
   preferences->channel = xfconf_channel_get ("xfce4-terminal");
 
   channels = xfconf_list_channels ();
-  if (!g_strv_contains ((const gchar * const *) channels, "xfce4-terminal"))
+  if (channels != NULL && !g_strv_contains ((const gchar * const *) channels, "xfce4-terminal"))
     {
       /* try to load the old config file & save changes */
       terminal_preferences_load_rc_file (preferences);