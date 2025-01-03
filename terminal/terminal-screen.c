f28e651004cf1eb2daf671591f6ea3d34bee507e
Gaël Bonithon <gael@xfce.org>
2025-01-03 17:07:26 +0100

    screen: Don't overwrite VteTerminal:allow-hyperlink binding
    It seems to have worked in the past despite this, perhaps thanks to a
    different initialization order. Today, this call overwrites the binding
    made in terminal_widget_init(), and it seems to serve no purpose.
    
    Fixes: #348

--- terminal/terminal-screen.c
+++ terminal/terminal-screen.c
@@ -401,8 +401,6 @@ terminal_screen_init (TerminalScreen *screen)
   terminal_screen_update_colors (screen);
   terminal_screen_update_sixel (screen);
 
-  vte_terminal_set_allow_hyperlink (VTE_TERMINAL (screen->terminal), TRUE);
-
   /* last, connect contents-changed to avoid a race with updates above */
   g_signal_connect_swapped (G_OBJECT (screen->terminal), "contents-changed",
       G_CALLBACK (terminal_screen_vte_window_contents_changed), screen);
