xfce-mirror/xfce4-terminal

commit c9167f5127be59d2fed34257da169c95f0bb21c0

Escape special characters in dnded uris
This resolves a vulnerability by breaking a few cases that could work
directly before, such as when the file name contains a tab or a line
break. But perhaps it is better to let the user manage these cases
manually as well, rather than having code interpreted on the fly during
drag and drop.

Fixes: #380

terminal/terminal-widget.c
@@ -756,6 +756,9 @@ terminal_widget_drag_data_received (GtkWidget *widget,
                   /* quote the file name (for the shell) */
                   uris[n] = g_shell_quote (filename);
                   g_free (filename);
+                  filename = g_strescape (uris[n], "\"\\");
+                  g_free (uris[n]);
+                  uris[n] = filename;
                 }
             }
 

commit eb2bbafa7966bab4a0cb0885956a078f8e4973b7

Do not escape non-ASCII characters in dnded uris
This prevents the direct use of paths containing accented characters,
smileys, etc., even though it is unnecessary from a security standpoint.

Fixes: c9167f5127be59d2fed34257da169c95f0bb21c0
Related: #380

terminal/terminal-widget.c
@@ -750,13 +750,25 @@ terminal_widget_drag_data_received (GtkWidget *widget,
               filename = g_filename_from_uri (uris[n], NULL, NULL);
               if (G_LIKELY (filename != NULL))
                 {
+                  /* exclude non-ASCII characters from escaping below */
+                  const gchar *excluded =
+                    "\"\\"
+                    "\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f"
+                    "\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f"
+                    "\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf"
+                    "\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf"
+                    "\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf"
+                    "\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf"
+                    "\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef"
+                    "\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff";
+
                   /* release the file:-URI */
                   g_free (uris[n]);
 
                   /* quote the file name (for the shell) */
                   uris[n] = g_shell_quote (filename);
                   g_free (filename);
-                  filename = g_strescape (uris[n], "\"\\");
+                  filename = g_strescape (uris[n], excluded);
                   g_free (uris[n]);
                   uris[n] = filename;
                 }