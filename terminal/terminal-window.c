xfce-mirror/xfce4-terminal

commit f75bb061b9d4168ed1bc82a8cfca919ecb9f8b5b

cleanup: Move function and set static qualifier

terminal/terminal-window.c
@@ -1689,6 +1689,9 @@ terminal_window_notebook_page_switched (GtkNotebook *notebook,
       /* store last and set new active tab */
       window->priv->last_active = window->priv->active;
       window->priv->active = active;
+      gtk_widget_show (GTK_WIDGET (window->priv->active));
+      if (window->priv->last_active != NULL)
+        gtk_widget_hide (GTK_WIDGET (window->priv->last_active));
 
       /* set the new window title */
       terminal_window_notify_title (active, NULL, window);

terminal/terminal-screen.c
@@ -2054,6 +2054,20 @@ terminal_screen_paste_unsafe_text (TerminalScreen *screen,
 
 
 static void
+terminal_screen_update_sixel (TerminalScreen *screen)
+{
+#if VTE_CHECK_VERSION(0, 61, 90)
+  gboolean enable_sixel;
+  g_object_get (G_OBJECT (screen->preferences),
+                "enable-sixel", &enable_sixel,
+                NULL);
+  vte_terminal_set_enable_sixel (VTE_TERMINAL (screen->terminal), enable_sixel);
+#endif
+}
+
+
+
+static void
 terminal_screen_spawn_async_cb (VteTerminal *terminal,
                                 GPid pid,
                                 GError *error,
@@ -3217,18 +3231,4 @@ terminal_screen_widget_append_accels (TerminalScreen *screen,
   g_return_if_fail (TERMINAL_IS_SCREEN (screen));
 
   g_object_set (G_OBJECT (screen->terminal), "accel-group", accel_group, NULL);
-}
-
-
-
-void
-terminal_screen_update_sixel (TerminalScreen *screen)
-{
-#if VTE_CHECK_VERSION(0, 61, 90)
-  gboolean enable_sixel;
-  g_object_get (G_OBJECT (screen->preferences),
-                "enable-sixel", &enable_sixel,
-                NULL);
-  vte_terminal_set_enable_sixel (VTE_TERMINAL (screen->terminal), enable_sixel);
-#endif
 }

commit 49716c1a41fbfe15ad1dd6c7694fa0d1289b6ac7

Fix previous commit
Fixes: f75bb061b9d4168ed1bc82a8cfca919ecb9f8b5b

terminal/terminal-window.c
@@ -1689,9 +1689,6 @@ terminal_window_notebook_page_switched (GtkNotebook *notebook,
       /* store last and set new active tab */
       window->priv->last_active = window->priv->active;
       window->priv->active = active;
-      gtk_widget_show (GTK_WIDGET (window->priv->active));
-      if (window->priv->last_active != NULL)
-        gtk_widget_hide (GTK_WIDGET (window->priv->last_active));
 
       /* set the new window title */
       terminal_window_notify_title (active, NULL, window);